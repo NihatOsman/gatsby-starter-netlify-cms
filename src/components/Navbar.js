14fe0ceafeb4b3d14e54bdc1be2e0a320eaeca04
nihat <nihatt1994@gmail.com>
2019-08-13 15:35:52 +0300

    added header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,98 +1,115 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../assets/logos/logo-shipcars.svg';
+import smallLogo from '../assets/logos/small-logo.svg';
+import DropDown from './DropDown';
 
-const Navbar = class extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      active: false,
-      navBarActiveClass: '',
-    }
-  }
 
-  toggleHamburger = () => {
-    // toggle the active boolean in the state
-    this.setState(
+import Button from '@material-ui/core/Button';
+const headerCongif = [
+  {
+    title: 'Products',
+    open: false,
+    menu: [
       {
-        active: !this.state.active,
+        title: 'Calcatron',
+        icon: 'calcatron',
+        to: '/calcatron-car-shipping-calculator',
+        description: 'Access our innovative Predicative Rate Engine'
       },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-              navBarActiveClass: 'is-active',
-            })
-          : this.setState({
-              navBarActiveClass: '',
-            })
+      {
+        title: 'Shipper TMS',
+        icon: 'shipper',
+        to: '/shipper-tms',
+        description: 'Search & post new loads daily'
+      },
+      {
+        title: 'Market Loadboard',
+        icon: 'market',
+        to: '/market-car-transport-loadboard',
+        description: 'Automate your vehicle transport workflows'
+      },
+      {
+        title: 'Carrier TMS',
+        icon: 'carrier',
+        to: '/carrier-tms',
+        description: 'Automate your vehicle shipping processes'
+      },
+      {
+        title: 'Driver ePOD',
+        icon: 'driver',
+        to: '/driver-epod',
+        description: 'Download our mobile app for drivers'
       }
-    )
+    ]
+  },
+  {
+    title: 'Use Cases',
+    class: 'use-case',
+    open: false,
+    menu: [
+      {
+        title: 'Shippers & Brokers',
+        icon: 'careers',
+        to: '/use-cases-brokers-shippers',
+        description:
+          'Optimize your business & increase productivity with automatic status updates and instant tracking'
+      },
+      {
+        title: 'Carriers & Drivers',
+        icon: 'carriers-drivers',
+        to: '/use-cases-carriers-drivers',
+        description:
+          'Discover how to make workplace operations securer and easier with our innovative tech & software'
+      }
+    ]
+  },
+  {
+    title: 'Company',
+    classes: 'company',
+    open: false,
+    menu: [
+      { title: 'About', icon: 'about', to: '/about' },
+      { title: 'Prices', icon: 'prices', to: '/prices' },
+      { title: 'Careers', icon: 'careers', to: '/careers' },
+      { title: 'Contact', icon: 'contact-us', to: '/' }
+    ]
   }
+];
 
-  render() {
-    return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
+
+
+export default props => {
+
+
+  return (
+    <header className={`header ${props.fixHeader ? 'fixed' : ''}`}>
+      <div>
+        <Link className="nav-item" to="/">
+          <img className="logo" src={logo} alt="ship cars" />
+          <img
+            className={`small-logo
+                ${props.fixHeader ? 'hide' : ''}`}
+            src={smallLogo}
+            alt="ship cars"
+          />
+        </Link>
+        <div className="navigation">
+                <div className="navbar-menu">
+                    {headerCongif.map((item, index) => (
+                        <DropDown item={item} key={index} />
+                    ))}
+                    <a className="nav-item" href="https://help.ship.cars/">
+                        Knowledge Base
+                    </a>
+                </div>
             </div>
-          </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </nav>
-    )
-  }
+       
+         
+
+      </div>
+    </header>
+  )
 }
 
-export default Navbar
+
